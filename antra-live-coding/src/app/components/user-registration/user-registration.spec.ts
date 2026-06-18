e529a536fd161e6aefd7530aafb86c4b4df2fae4
Ritik Shrivastava <shriva23@purdue.edu>
2026-06-18 10:29:37 -0500

    added optional existing username check in registration component

--- antra-live-coding/src/app/components/user-registration/user-registration.spec.ts
+++ antra-live-coding/src/app/components/user-registration/user-registration.spec.ts
@@ -1,3 +1,4 @@
+import { provideHttpClient } from '@angular/common/http';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { UserRegistration } from './user-registration';
@@ -9,11 +10,12 @@ describe('UserRegistration', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [UserRegistration],
+      providers: [provideHttpClient()]
     }).compileComponents();
 
     fixture = TestBed.createComponent(UserRegistration);
     component = fixture.componentInstance;
-    await fixture.whenStable();
+    fixture.detectChanges();
   });
 
   it('should create', () => {
